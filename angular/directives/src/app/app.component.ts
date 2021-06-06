3f17cdf5c7e5bfd97b5b86201e42fa19fad0aeeb
steevenson paul <paul@steevenson.fr>
2021-06-06 18:32:39 +0200

    [angular]: Playing with attributes directives

--- angular/directives/src/app/app.component.ts
+++ angular/directives/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'directives';
+export class AppComponent implements OnInit {
+    title = 'directives';
+     color = '';
+    currentClasses: Record<string, boolean> = {};
+
+    isSpecial = true;
+    canSave = true;
+    isUnchanged = false;
+
+    currentItem = { name: '', upperCaseName: '' };
+
+    ngOnInit() {
+        this.setCurrentClasses();
+    }
+
+    setCurrentClasses() {
+        this.currentClasses = {
+            saveable: this.canSave,
+            modified: !this.isUnchanged,
+            special: this.isSpecial
+        }
+    }
+
+    onModelChanges($event: string) {
+        this.currentItem.upperCaseName = $event.toUpperCase();
+    
+    }
 }
