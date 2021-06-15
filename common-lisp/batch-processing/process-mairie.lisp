eb9ef233f9a8aa66a68966a2862c52fca76d5b6d
steevenson paul <paul@steevenson.fr>
2021-06-16 00:41:36 +0200

    [batch-processing]: Add a new function to read file at once

--- common-lisp/batch-processing/process-mairie.lisp
+++ common-lisp/batch-processing/process-mairie.lisp
@@ -3,14 +3,22 @@
         (code-postal (read-stream:up-to stream #\)))
         maire)
     (read-stream:up-to stream #\-)
-    (setf maire (read-stream:up-to-end stream))
+    (setf maire (read-stream:up-to stream #\NewLine))
     (list :city (string-downcase (string-trim " " (coerce city 'string)))
           :maire (string-trim " " (coerce maire 'string))
           :code-postal (coerce code-postal 'string))))
 
 (defun process-mairie ()
   (with-open-file (in "mairie.data")
-    (loop for (line no-nl-p)
-            = (multiple-value-list (read-line in nil nil))
+    (loop for line = (read-line in nil nil)
           while line
           collect (parse-mairie (read-stream:read-stream-on line)))))
+
+(defun process-at-once (stream)
+  (labels ((at-once (result)
+             (if (read-stream:at-end-p stream)
+                 result
+                 (progn
+                   (push (parse-mairie stream) result)
+                   (at-once result)))))
+    (at-once '())))
