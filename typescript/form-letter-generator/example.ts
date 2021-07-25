767858f79d73d7aa0cb79bdd168a57db83d7108b
steevenson paul <paul@steevenson.fr>
2021-07-25 21:29:57 +0200

    [typescript]: Playing with another data format

--- typescript/form-letter-generator/example.ts
+++ typescript/form-letter-generator/example.ts
@@ -1,4 +1,7 @@
+import { ReadStream, streamOn, upTo } from 'flotjs';
 import { stInterpol } from './form-letter-generator';
+
+
 
 
 const template = `(:first-name %firstName :last-name "%lastName" :age %age)`;
@@ -12,3 +15,24 @@ const data = {
 
 
 console.log(stInterpol(template, data).text);
+
+const s = streamOn(":nom paul :prenom steevenson :naissance 21/01/1989");
+
+function processKeywordFormat(stream: ReadStream) {
+    const recur: (result: any[], isAtEnd: boolean) => any[] = (result: any[], isAtEnd: boolean) => {
+        if (isAtEnd) {
+            return result;
+        } else {
+            result.push(upTo(stream, ":"));
+            return recur(result, s.atEnd());
+        }
+    }
+
+    return recur([], stream.atEnd());
+}
+
+type Enfant = {
+    nom: string;
+    prenom: string;
+    naissance: string;
+}
