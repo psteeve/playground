8f6e45d11cbb9535d29945695f8c0ed9b5c384b3
steevenson paul <paul@steevenson.fr>
2021-08-09 17:06:40 +0200

    [racket]: Playing with racket

---
+++ racket/gui.rkt
@@ -0,0 +1,19 @@
+#lang racket/gui
+
+(define frame (new frame% [label "Guess"]))
+
+(define secret (random 5))
+
+(define ((check i) btn evt)
+  (define found? (if (= i secret) "Yes" "No"))
+  (message-box "?" found?)
+  (when (= i secret)
+    (send frame show #false)))
+
+(for ([i (in-range 5)])
+  (new button%
+       [label (~a i)]
+       [parent frame]
+       [callback (check i)]))
+
+(send frame show #t)
