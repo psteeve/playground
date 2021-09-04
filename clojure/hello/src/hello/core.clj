6aebf633f4079cc847384ac772afad8c87baf011
steevenson paul <paul@steevenson.fr>
2021-09-04 20:56:55 +0200

    [clojure]: Playing with it

--- clojure/hello/src/hello/core.clj
+++ clojure/hello/src/hello/core.clj
@@ -3,3 +3,11 @@
 (defn factorial [x]
   (if (= x 0) 1
       (* x (factorial (- x 1)))))
+
+(def first-name "Dave")
+
+(str "I'm sorry " first-name)
+
+(def ages [42 3 18])
+
+(defn greetings [name] (str "hello Mr. " name))
